f51dcb61eba1a290cdf6fc99cd801c72476ab587
Cristian Colorado <cristian.colorado@hbr.org>
2018-04-06 15:40:42 -0500

    Fix: Adding hashtag selection based on weekdays

--- services/cron/scripts/tweet-by-trend.js
+++ services/cron/scripts/tweet-by-trend.js
@@ -1,15 +1,20 @@
 const Twitter = require('commons').Twitter;
 const twitterAPI = new Twitter();
-const weekday = require('./data/weekday');
+const weekday = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sabado', 'domingo'];
 
 console.log(`>> ${new Date()} - Searching for top trending`);
 
 twitterAPI.trends('23424900').then((trends) => {
-  let filterTrends = trends.filter(trend => !weekday.includes(trend.name));
-  let trend = filterTrends[0];
-  let tweet = `¿Sabes que diputado te representa? #ContactoLegislativo te ayuda a ubicar y visualizar su desempeño de manera sencilla ${trend.name} https://contactolegislativo.com`;
+  // Iterate all trends
+  let nonWeekdayTrend = trends.find(trend => {
+    // If current trend contains a weekday, we will NOT pick that trend
+    return !weekday.some(day => trend.name.toLowerCase().indexOf(day) > -1);
+  })
+
+  let hashtag = nonWeekdayTrend ? nonWeekdayTrend.name : '#SomosCiudadanosResponsables';
+  let tweet = `¿Sabes que diputado te representa? #ContactoLegislativo te ayuda a ubicar y visualizar su desempeño de manera sencilla ${hashtag} https://contactolegislativo.com`;
   console.log(`>> ${tweet}`);
-  twitterAPI.tweet(tweet).then(() => {
-    console.log('>> Tweet successfull');
-  });
+  // twitterAPI.tweet(tweet).then(() => {
+  //   console.log('>> Tweet successfull');
+  // });
 });
--- services/cron/scripts/tweet-happy-weekday.js
+++ services/cron/scripts/tweet-happy-weekday.js
@@ -1,13 +1,21 @@
 const Twitter = require('commons').Twitter;
-const weekday = require('./data/weekday');
+const weekdayHashtag = require('./data/weekday');
+const weekday = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sabado', 'domingo'];
 const twitterAPI = new Twitter();
 
-console.log(`>> ${new Date()} - Calculating weekday`);
+const date = new Date();
+console.log(`>> ${date} - Calculating weekday`);
 
-const d = new Date();
-const hashtag = weekday[d.getDay()];
+twitterAPI.trends('23424900').then((trends) => {
+  // Iterate all trends
+  let weekdayTrend = trends.find(trend => {
+    // If current trend contains a weekday, we will pick that trend
+    return weekday.some(day => trend.name.toLowerCase().indexOf(day) > -1);
+  })
 
-twitterAPI.trends('23424900').then((trend) => {
+  // If trend found display trend, otherwise display predefined hashtag
+  let hashtag = weekdayTrend ? weekdayTrend.name : weekdayHashtag[date.getDay()];
+
   let tweet = `¿Sabes que diputado te representa? #ContactoLegislativo te ayuda a ubicar y visualizar su desempeño de manera sencilla ${hashtag} https://contactolegislativo.com`;
   console.log(`>> ${tweet}`);
   twitterAPI.tweet(tweet).then(() => {
