d593283ffcd722b99b9078ec0fc37c25625972dd
Cristian Colorado <ccoloradoc@gmail.com>
2018-04-07 18:13:47 -0500

    Fix: Changing hour

--- services/cron/cron.js
+++ services/cron/cron.js
@@ -7,7 +7,7 @@ console.log(`>> ${new Date()} Service up`);
 let cronjobs = [
   { name: 'weekday', schedule: '00 20 09 * * *', command: 'node scripts/tweet-happy-weekday.js >> log/happy.log'},
   { name: 'trend', schedule: '00 53 13 * * *', command: 'node scripts/tweet-by-trend.js >> log/trend.log'},
-  { name: 'deputy', schedule: '00 32 18 * * *', command: 'node scripts/tweet-by-deputy.js >> log/deputy.log'}
+  { name: 'deputy', schedule: '00 15 17 * * *', command: 'node scripts/tweet-by-deputy.js >> log/deputy.log'}
 ];
 
 cronjobs.forEach(cronjob => {
