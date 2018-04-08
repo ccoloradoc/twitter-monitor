02c87c35570d2f2173193c5709f1d9456668c9e7
Cristian Colorado <ccoloradoc@gmail.com>
2018-04-07 22:13:35 -0500

    Fix: Adding constant

--- services/commons/services/facebook.js
+++ services/commons/services/facebook.js
@@ -1,6 +1,8 @@
 const config = require('../config');
 const axios = require('axios');
 
+const FACEBOOK_API = 'https://graph.facebook.com/v2.8';
+
 class FacebookApi {
   post(message, link) {
     return axios.post(`${FACEBOOK_API}/${config.fb.page_id}/feed`, {
