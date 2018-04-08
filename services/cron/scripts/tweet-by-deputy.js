8bff4bda796e7eda4daf013d16e3d54695c2374c
Cristian Colorado <ccoloradoc@gmail.com>
2018-04-07 22:19:31 -0500

    Fix: Removing tweet reference from fb post

--- services/cron/scripts/tweet-by-deputy.js
+++ services/cron/scripts/tweet-by-deputy.js
@@ -37,11 +37,12 @@ monitorService.findAllDeputies({ type: 'Mayoría Relativa' })
     let distribution = frequencyDistribution(deputy, frequency);
     let position = quote(distribution);
     let account = deputy.twitter ? `@${deputy.twitter}`: '';
-    let message = `Te presentamos a ${deputy.displayName} ${account} diputado del distrito ${deputy.area} de #${deputy.state.replace(' ', '')} #${deputy.party.toUpperCase()}, con ${deputy.attendances}/165 asistencias ${position}, conoce mas sobre tu diputado en #ContactoLegislativo`;
+    let tweet = `Te presentamos a ${deputy.displayName} ${account} diputado del distrito ${deputy.area} de #${deputy.state.replace(' ', '')} #${deputy.party.toUpperCase()}, con ${deputy.attendances}/165 asistencias ${position}, conoce mas sobre tu diputado en #ContactoLegislativo`;
+    let message = `Te presentamos a ${deputy.displayName} diputado del distrito ${deputy.area} de #${deputy.state.replace(' ', '')} #${deputy.party.toUpperCase()}, con ${deputy.attendances}/165 asistencias ${position}, conoce mas sobre tu diputado en #ContactoLegislativo`;
     let link = `https://contactolegislativo.com/camara-de-diputados/LXIII/${deputy.slug}`;
 
     console.log(`>> ${message} ${link}`);
-    twitterAPI.tweet(`${message} ${link}`).then(() => {
+    twitterAPI.tweet(`${tweet} ${link}`).then(() => {
       deputy.lastPublishedDate = new Date();
       deputy.save(() => {
         console.log(`>> Tweet successfull & deputy saved`);
