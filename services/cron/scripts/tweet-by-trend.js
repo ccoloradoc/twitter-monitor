f57620f6e3d0299ab87d8dc5cd1cfdab19eec213
Cristian Colorado <ccoloradoc@gmail.com>
2018-04-23 20:00:33 -0500

    Fix: Adding candidates to black list

--- services/cron/scripts/tweet-by-trend.js
+++ services/cron/scripts/tweet-by-trend.js
@@ -1,14 +1,15 @@
 const Twitter = require('commons').Twitter;
 const twitterAPI = new Twitter();
-const weekday = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sabado', 'domingo'];
+const blackList =
+  ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sabado', 'domingo', 'amlo', 'anaya', 'meade', 'frente', 'historia', 'juntos'];
 
 console.log(`>> ${new Date()} - Searching for top trending`);
 
 twitterAPI.trends('23424900').then((trends) => {
   // Iterate all trends
   let nonWeekdayTrend = trends.find(trend => {
-    // If current trend contains a weekday, we will NOT pick that trend
-    return !weekday.some(day => trend.name.toLowerCase().indexOf(day) > -1);
+    // If current trend contains a blackList, we will NOT pick that trend
+    return !blackList.some(day => trend.name.toLowerCase().indexOf(day) > -1);
   })
 
   let hashtag = nonWeekdayTrend ? nonWeekdayTrend.name : '#SomosCiudadanosResponsables';
